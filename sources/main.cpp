c888fa919e1c20581fedb3e5a8c0aae6a2503451
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-23 02:35:46 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,11 @@
 // Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
-#include <iostream>               // подключаем заголовочный файл iostream
-int main()                          // определяем функцию main
-{                                   // начало функции
-    std::cout << "Hello World!";  // выводим строку на консоль
-    return 0;                       // выходим из функции
-}                                   // конец функции
+#include <iostream>//
+int main()//
+{ //
+    char  s;//
+    std::cout << "write symbol: ";//
+    std::cin >> s ; //
+    std::cout << "\n"; //empty string
+    std::cout << "1 13 49\n" << "1" << s << "13" << s << "49 "; //
+    return 0; //
+}
