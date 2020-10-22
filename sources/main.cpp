dbb9b51d6f7509a8c67af9d6f225410037116d01
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-22 22:15:49 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>               // подключаем заголовочный файл iostream
-int main()                          // определяем функцию main	int main()                          // определяем функцию main
-{                                   // начало функции	{                                   // начало функции
-    std::cout << "Hello World!";  // выводим строку на консоль	    std::cout << "Hello World!";  // выводим строку на консоль
+#include <iostream>              
+int main()                         
+{                                   
+    std::cout << "Hello World!";  
