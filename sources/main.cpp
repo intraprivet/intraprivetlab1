8a5d154793b9b49da00b1322aa3529e687b6c45b
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-23 01:03:28 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,9 @@
 // Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
 #include <iostream>
 int main()
-{
-    char  s;
-    std::cout << "write symbol: ";
-    std::cin >> s ;
-    std::cout << "\n"; //empty string
-    std::cout << "1 13 49\n" << "1" << s << "13" << s << "49 ";
+{char  s;
+ std::cout << "write symbol: ";
+ std::cin >> s ;
+ std::cout << "\n"; //empty string
+ std::cout << "1 13 49\n" << "1" << s << "13" << s << "49 ";
 }
