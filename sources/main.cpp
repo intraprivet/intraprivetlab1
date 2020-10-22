c81550d1c4b4cc922778157880a7449bc5fe5bf9
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-22 22:39:46 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,7 @@
 // Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
-#include <iostream>              // подключаем заголовочный файл  cstdio
-int main()                          // определяем функцию main
-{                                   // начало функции
-    std::cout << "1 13 49";  // выводим строку на консоль
-    return 0;                       // выходим из функции
-}                                   // конец функции
+#include <iostream>
+#include <cstdlib>
+int main()
+{
+    std::cout << "1 13 49 ";
+}
