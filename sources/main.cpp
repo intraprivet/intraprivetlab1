a36c6a56fa6639b369baf454917b42b2f4f0ea91
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-22 22:31:44 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,7 @@
 // Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+#include <cstdio>              // подключаем заголовочный файл  cstdio
+int main()                          // определяем функцию main
+{                                   // начало функции
+    std::cout << "1 13 49";  // выводим строку на консоль
+    return 0;                       // выходим из функции
+}                                   // конец функции
