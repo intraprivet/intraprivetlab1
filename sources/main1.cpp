e36b1e903cc96bb5cbc4990c9f0fffdf21f75a22
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-23 00:41:46 +0300

    Create main1.cpp

---
+++ sources/main1.cpp
@@ -0,0 +1,17 @@
+// Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
+#include <iostream>
+int main()
+{
+    char  s;
+    std::cout << "write symbol: ";
+    std::cin >> s ;
+    std::cout << "\n"; //empty string
+    std::cout << "1 13 49\n" << "1" << s << "13" << s << "49 ";
+
+    
+    char  d;
+    std::cout << "write symbol: ";
+    std::cin >> d ;
+    std::cout << "\n"; //empty string
+    std::cout << "3 3 3\n" << "3" << s << "3" << s << "3 ";
+}
