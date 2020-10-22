ff8d71169ab725cad9e21393d00bfa3e405a4e72
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-23 01:11:33 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -8,3 +8,10 @@ int main()
  std::cout << "\n"; //empty string
  std::cout << "1 13 49\n" << "1" << s << "13" << s << "49 ";
 }
+// Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
+
+#include <iostream>               // подключаем заголовочный файл iostream
+int main()                          // определяем функцию main	int main()                          // определяем функцию main
+{                                   // начало функции	{                                   // начало функции
+    std::cout << "Hello World!";  // выводим строку на консоль	    std::cout << "Hello World!";  // выводим строку на консоль
+}
