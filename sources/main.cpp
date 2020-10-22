7d597c88ea2abdb0d17208ad9ee7897ce413b9d7
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-23 01:11:56 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,14 +1,4 @@
 // Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
-#include <iostream>
-int main()
-{
- char  s;
- std::cout << "write symbol: ";
- std::cin >> s ;
- std::cout << "\n"; //empty string
- std::cout << "1 13 49\n" << "1" << s << "13" << s << "49 ";
-}
-// Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
 
 #include <iostream>               // подключаем заголовочный файл iostream
 int main()                          // определяем функцию main	int main()                          // определяем функцию main
