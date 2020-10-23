902ba239b85edecfa27afcee7e29d3f5832860a0
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-23 03:07:14 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,8 +1,8 @@
 // Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
 #include <iostream>               
 int main()                          
-{                                  
+{                                  //1
     std::cout << "Hello World!";  
-    return 0;                       
-}                                  
+    return 0;                     
+}              //2                    
 
