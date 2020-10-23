ff60a7fd1e5eca141d1d78ecc81ee3044ebab7b3
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-23 03:16:09 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,8 +1,7 @@
 // Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
-#include <iostream>               
-int main()                          
-{                                  //1
-    std::cout << "Hello World!";  
-    return 0;                     
-}              //2                    
-
+#include <iostream>               // подключаем заголовочный файл iostream
+int main()                          // определяем функцию main
+{                                   // начало функции
+    std::cout << "Hello World!";  // выводим строку на консоль
+    return 0;                       // выходим из функции
+}                                   // конец функции
