a8f2f697b2f8f2dfc71c36ebebe64152b4f3f9d8
intraprivet <72947916+intraprivet@users.noreply.github.com>
2020-10-23 03:28:46 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,8 +1,10 @@
 // Copyright 2020 Alex Pushkin pushkin.alexsandt@gmail.com
-#include <iostream>               // 1
-int main()                          
-{                                   // 3
-    std::cout << "Hello World!";  // 4
-    return 0;                       // 5
-}                                   // 6
-
+#include <iostream>                                                      //1
+int main()                                                               //2
+{                                                                        //3
+    char  s;                                                             //4
+    std::cout << "write symbol: ";                                       //5
+    std::cin >> s ;                                                      //6
+    std::cout << "\n";                                                   //7 
+    std::cout << "1 13 49\n" << "1" << s << "13" << s << "49 ";          //8
+}
